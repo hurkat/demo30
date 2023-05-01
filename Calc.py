51f2d6697cf5b984d4a4872b24723705eefa02e2
Harshit Mishra <hmharsh123@gmail.com>
2023-05-01 12:11:36 +0530

    3RD COMMIT

--- Calc.py
+++ Calc.py
@@ -1,17 +1,21 @@
 import Addition
 import Subtraction
+import Multiplication
+import Division
 
 # This is to present a menu to the user
 print("Select operation.")
 print("1.Addition")
 print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2'):
+    if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -24,6 +28,12 @@ while True:
 
         elif choice == '2':
             print(num1, "-", num2, "=", Subtraction.subtract(num1, num2))
+        elif choice == '3':
+            print(num1, "*", num2, "=", Multiplication.multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", Division.divide(num1, num2))
+        
 
         
         
